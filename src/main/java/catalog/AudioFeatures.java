ac4e50612548920ca74c1d93da5ce254daae803e
Péter Bárdos <bardospetikmet@gmail.com>
2018-07-12 11:30:48 +0200

    audio & printed features classes done without exceptions

--- src/main/java/catalog/AudioFeatures.java
+++ src/main/java/catalog/AudioFeatures.java
@@ -1,15 +1,44 @@
 package catalog;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AudioFeatures implements Feature {
+
+    private List<String> composer;
+    private int length;
+    private List<String> performers;
+    private String title;
+
+    public AudioFeatures(List<String> composer, int length, String title) {
+        // kivételkezelés
+        this.composer = composer;
+        this.length = length;
+        this.title = title;
+    }
+
+    public AudioFeatures(List<String> composer, int length, List<String> performers, String title) {
+        // kivételkezelés
+        this.composer = composer;
+        this.length = length;
+        this.performers = performers;
+        this.title = title;
+    }
+
     @Override
     public List<String> getContributors() {
-        return null;
+        // kivételkezelés
+        List<String> contributors = new ArrayList<>(composer);
+        contributors.addAll(performers);
+        return contributors;
+    }
+
+    public int getLength() {
+        return length;
     }
 
     @Override
     public String getTitle() {
-        return null;
+        return title;
     }
 }
--- src/main/java/catalog/PrintedFeatures.java
+++ src/main/java/catalog/PrintedFeatures.java
@@ -3,13 +3,30 @@ package catalog;
 import java.util.List;
 
 public class PrintedFeatures implements Feature {
+
+    private List<String> authors;
+    private int numberOfPages;
+    private String title;
+
+    public PrintedFeatures(List<String> authors, int numberOfPages, String title) {
+        // kivételkezelés
+        this.authors = authors;
+        this.numberOfPages = numberOfPages;
+        this.title = title;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
     @Override
     public List<String> getContributors() {
-        return null;
+        // kivételkezelés
+        return authors;
     }
 
     @Override
     public String getTitle() {
-        return null;
+        return title;
     }
 }
