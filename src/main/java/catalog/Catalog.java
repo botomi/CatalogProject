1184ad6606d2e41734960e61913c318d7acb7e0e
Péter Bárdos <bardospetikmet@gmail.com>
2018-07-12 13:14:19 +0200

    Catalog first commit

--- src/main/java/catalog/Catalog.java
+++ src/main/java/catalog/Catalog.java
@@ -1,4 +1,79 @@
 package catalog;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Catalog {
+
+    List<CatalogItem> catalogItems = new ArrayList<>();
+
+    public void addItem(CatalogItem catalogItem) {
+        catalogItems.add(catalogItem);
+    }
+
+    public double averagePageNumberOver(int pageNumber) {
+
+
+        return 0.0;
+    }
+
+    public void deleteItemByRegistrationNumber(String registrationNumber) {
+
+
+
+//        return getRegistrationNumber;
+    }
+
+    public List<CatalogItem> findByCriteria(SearchCriteria searchCriteria) {
+
+
+
+
+        return null;
+    }
+
+    public int getAllPageNumber() {
+
+
+
+        return 0;
+    }
+
+    public List<CatalogItem> getAudioLibraryItems() {
+
+
+
+
+        return null;
+    }
+
+    public int getFullLength() {
+
+
+
+
+
+        return 0;
+    }
+
+    public List<CatalogItem> getPrintedLibraryItems() {
+
+
+
+
+        return null;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
