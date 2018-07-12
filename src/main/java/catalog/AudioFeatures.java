b637ea8f99c38fe7c5d864e6b3cb5eb0487dc993
Péter Bárdos <bardospetikmet@gmail.com>
2018-07-12 11:21:19 +0200

    classes done

---
+++ src/main/java/catalog/AudioFeatures.java
@@ -0,0 +1,15 @@
+package catalog;
+
+import java.util.List;
+
+public class AudioFeatures implements Feature {
+    @Override
+    public List<String> getContributors() {
+        return null;
+    }
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+}
