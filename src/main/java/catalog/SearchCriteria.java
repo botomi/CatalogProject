7586c887a7db2a8d1017a36f26e92e528b8af014
botomi <tamasbodnar@hotmail.com>
2018-07-12 11:32:05 +0200

    validator done

--- src/main/java/catalog/SearchCriteria.java
+++ src/main/java/catalog/SearchCriteria.java
@@ -1,4 +1,37 @@
 package catalog;
 
 public class SearchCriteria {
+
+    private String contributor;
+    private String title;
+
+    private SearchCriteria(String contributor, String title) {
+        this.contributor = contributor;
+        this.title = title;
+    }
+
+    private SearchCriteria(String contributor) {
+        this.contributor = contributor;
+    }
+
+    private SearchCriteria(String title) {
+        this.title = title;
+    }
+
+
+
+    public static SearchCriteria createByBoth(String title, String contributor) {
+        if (Validators.isBlank(title) || Validators.isBlank(contributor)) {
+            throw new IllegalArgumentException("Parameter must not be null!");
+        }
+        return new SearchCriteria(contributor, title);
+    }
+
+    public static SearchCriteria createByContributor(String contributor){
+        if (Validators.isBlank(contributor)) {
+            throw new IllegalArgumentException("Parameter must not be null!");
+        }
+
+
+    }
 }
--- src/main/java/catalog/Validators.java
+++ src/main/java/catalog/Validators.java
@@ -1,4 +1,20 @@
 package catalog;
 
+import java.util.List;
+
 public class Validators {
+
+    public static boolean isBlank(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isEmpty(List<Object> list) {
+        if (list == null || list.size() == 0) {
+            return true;
+        }
+        return false;
+    }
 }
